3ff2e44213f4c1a3efdc2c056e9731d24a767e5e
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-02-24 12:17:39 -0800

    added links for riley project

--- src/routes/music/projects/riley.jsx
+++ src/routes/music/projects/riley.jsx
@@ -23,38 +23,36 @@ export const Riley = ({ i, expanded, HandleActiveArtist }) => {
         titleComponent={<Title artistName="riley" subtitle="artist" />}
         discography={
           <Discography>
-            {/* TODO: get all links */}
             <Discography.Disc
               artwork={starsRemix}
               title="Stars (riley remix)"
-              appleMusicLink=""
-              spotifyLink=""
+              appleMusicLink="https://music.apple.com/us/album/stars-riley-remix/1660688944?i=1660688945"
+              spotifyLink="https://open.spotify.com/track/29NlMvw2a5h7o5sCqgJ7K3?si=f8ae2b4e9aa145b1"
               tidalLink=""
-              youtubeLink=""
+              youtubeLink="https://www.youtube.com/watch?v=9z8t3nt7ZmA"
             />
             <Discography.Disc
               artwork={iWas9Remix}
               title="I Was 9 (riley remix)"
-              appleMusicLink=""
-              spotifyLink=""
-              tidalLink=""
-              youtubeLink=""
+              appleMusicLink="https://music.apple.com/us/album/i-was-9-riley-remix/1649318275?i=1649318276"
+              spotifyLink="https://open.spotify.com/track/3F87Dak8Q41QSNbJfA6AMx?si=14fdcbed18374a3e"
+              tidalLink="https://tidal.com/browse/album/253512701"
+              youtubeLink="https://youtu.be/CsQ9kl_a1Y4?si=DOJsUs4qZvEmiplA"
             />
             <Discography.Disc
               artwork={better}
               title="Better (with John White and riley)"
-              appleMusicLink=""
-              spotifyLink=""
-              tidalLink=""
-              youtubeLink=""
+              appleMusicLink="https://music.apple.com/us/album/better-single/1556313448"
+              spotifyLink="https://open.spotify.com/track/52lu5hXrnYdWtPb90ImyA6?si=84018c33fb16478d"
+              tidalLink="https://tidal.com/browse/track/244622029"
+              youtubeLink="https://youtu.be/YkTWodHhM0o?si=ANBcBe_cGghTjFpJ"
             />
             <Discography.Disc
               artwork={losingHearts}
               title="Losing Hearts (feat. John White)"
-              appleMusicLink=""
-              spotifyLink=""
-              tidalLink=""
-              youtubeLink=""
+              appleMusicLink="https://music.apple.com/us/album/losing-hearts-feat-john-white/1509147409?i=1509147412"
+              spotifyLink="https://open.spotify.com/track/3lLtkLtBztQd8DiLCAORH5?si=f55f60cc305049c8"
+              youtubeLink="https://youtu.be/AloaubmwGEA?si=Mn_ZeImQwfpXb_z8"
             />
           </Discography>
         }
