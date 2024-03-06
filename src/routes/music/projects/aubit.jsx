356c796f7691a5ab80510dc7f218a9811e5d8130
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-03-05 21:27:43 -0800

    todo: add more links

--- src/routes/music/projects/aubit.jsx
+++ src/routes/music/projects/aubit.jsx
@@ -7,19 +7,21 @@ import aubitLogo from "/src/assets/images/projects/aubit-sound/aubit-logo.jpg";
 import odessaVol3 from "/src/assets/images/projects/aubit-sound/discography/odessa-vol-3.jpg";
 import odessaVol2 from "/src/assets/images/projects/aubit-sound/discography/odessa-vol-2.jpg";
 import awake from "/src/assets/images/projects/aubit-sound/discography/awake-vol-1.jpg";
+import garryx from "/src/assets/images/projects/aubit-sound/discography/garryx-for-serum-vol-1.jpg";
+import grayVol2 from "/src/assets/images/projects/aubit-sound/discography/gray-vol-2.jpg";
+import grayVol1 from "/src/assets/images/projects/aubit-sound/discography/gray-vol-1.jpg";
+import broox from "/src/assets/images/projects/aubit-sound/discography/broox-bounce-vol-1.jpg";
+import chainPop from "/src/assets/images/projects/aubit-sound/discography/chain-pop-vol-1.jpg";
 import the1975 from "/src/assets/images/projects/aubit-sound/discography/1975-for-serum.jpg";
 
-// broox
-// chain-pop
-// garryx
 // jacc
 // louv
 // masa
 // petit
 // plume
 // snakes
-// gray 2
-// gray 1
+// gray 2 *links!
+// gray 1 *links!
 // ultrallenium
 
 export const AubitSound = ({ i, expanded, HandleActiveArtist }) => {
@@ -51,6 +53,43 @@ export const AubitSound = ({ i, expanded, HandleActiveArtist }) => {
               title={"ODESSA Vol. 3"}
               webLink={"https://www.aubitsound.com/soundbanks/odessa-vol-3"}
               youtubeLink={"https://www.youtube.com/watch?v=_gMnFRN7WDw"}
+            />
+            <Discography.Disc
+              artwork={garryx}
+              title={"Garyx Vol. 1"}
+              soundcloudLink={
+                "https://soundcloud.com/synthpresets/aubit-sound-garyx-vol-1-serum-presets-midi-samples"
+              }
+              webLink={"https://www.aubitsound.com/soundbanks/garyx-vol-1"}
+              youtubeLink={"https://www.youtube.com/watch?v=SMjeEuZqL04"}
+            />
+            <Discography.Disc
+              artwork={grayVol2}
+              title={"Gray Vol. 2"}
+              webLink={"https://www.aubitsound.com/soundbanks/gray-vol-2"}
+            />
+            <Discography.Disc
+              artwork={grayVol1}
+              title={"Gray Vol. 1"}
+              webLink={"https://www.aubitsound.com/soundbanks/gray-vol-1"}
+            />
+            <Discography.Disc
+              artwork={chainPop}
+              title={"Chain-Pop Vol. 1"}
+              soundcloudLink={
+                "https://soundcloud.com/aubitofficial/chain-pop-vol-1"
+              }
+              webLink={"https://www.aubitsound.com/soundbanks/chain-pop-vol-1"}
+              youtubeLink={"https://www.youtube.com/watch?v=2Nxc97Dsnws"}
+            />
+            <Discography.Disc
+              artwork={broox}
+              title={"Broox Bounce Vol. 1"}
+              soundcloudLink={"https://on.soundcloud.com/fm3dn"}
+              webLink={
+                "https://www.adsrsounds.com/product/presets/broox-bounce-vol-1/"
+              }
+              youtubeLink={"https://www.youtube.com/watch?v=hit_3cN9z6g"}
             />
             <Discography.Disc
               artwork={awake}
