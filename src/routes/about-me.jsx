8db6a8a6c732718ebf4e31edb8ff9e582895f589
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-03-06 12:35:42 -0800

    updated about-me

--- src/routes/about-me.jsx
+++ src/routes/about-me.jsx
@@ -6,13 +6,14 @@ const AboutMe = () => {
       <p className="text-justify text-lg mx-6 md:px-24 lowercase">
         I&apos;m a music producer, sound designer, composer, arranger,
         songwriter, and recording artist. I&apos;ve been messing around with
-        beats for over 12 years, blending digital and analog stuff to make
-        sounds that are nostalgic, but still fresh. I love using sounds and
+        beats and sounds for over 12 years, blending digital and analog stuff to
+        make sounds that are nostalgic, but still fresh. I love using sounds and
         tones from all sorts of gadgets, like computer processors and even smart
-        ovens. Connect with me and let&apos;s journey into more uncharted sound
-        territories, where old meets new, and we cook up this symphony of
-        whatever feels right. Feel free to poke around my projects——they all
-        poke around a computer&apos;s emotions, you know, bit by bit.
+        ovens. You can hear my work all over the place—from small artists, big
+        artists, even Adidas commercials. If I&apos;m not doing music, I&apos;m
+        probably either writing code or playing video games. Feel free to poke
+        around my projects——I&apos;m really proud of my portfolio so far and I
+        love collaborating, so reach out if my stuff interests you!
       </p>
     </div>
   );
