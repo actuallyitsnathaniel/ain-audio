e3d913d65fbee09fa334c436f47cc04295353af2
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-03-05 19:02:52 -0800

    refactored for cleaner project implementation

--- src/routes/music/projects.jsx
+++ src/routes/music/projects.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { SamDenton } from "./projects/sam-denton";
 import { Ryland } from "./projects/ryland";
 import { Riley } from "./projects/riley";
@@ -20,30 +20,40 @@ const ProjectHighlights = () => {
   };
   const [expanded, setExpanded] = useState(-1);
 
+  const children = [
+    <SamDenton key="sam-denton" />,
+    <Ryland key="ryland" />,
+    <AubitSound key="aubit-sound" />,
+    <JohnWhite key="john-white" />,
+    <Riley key="riley" />,
+    <BrandX key="brand-x" />,
+    <KRPTK key="krptk" />,
+    <PlatinumRoses key="platinum-roses" />,
+  ];
+
+  /* 
+    TODO: literally the rest. go through projects PDF.
+    - AubitSound
+      - apply links to rest of projects
+    - PlatinumRoses
+      - LINKS!!!
+    - ADIDAS!!! * ask jake about stuff here.
+  */
+
   return (
     <div id="projects" className="md:pt-16">
       <h1 className="p-5 flex justify-center text-4xl">project highlights</h1>
       <div id="projects" className="flex flex-wrap justify-center">
         {/* TODO: integrate socials for each artist, just under title
             TODO: add descriptions for each artist, under socials */}
-        <SamDenton i={0} {...{ expanded, HandleActiveArtist }} />
-        <Ryland i={1} {...{ expanded, HandleActiveArtist }} />
-        <AubitSound i={2} {...{ expanded, HandleActiveArtist }} />
-        <JohnWhite i={3} {...{ expanded, HandleActiveArtist }} />
-        {/** Will need to add hyperpop EP soon */}
-        <Riley i={4} {...{ expanded, HandleActiveArtist }} />
-        <BrandX i={5} {...{ expanded, HandleActiveArtist }} />
-        <KRPTK i={6} {...{ expanded, HandleActiveArtist }} />
-        <PlatinumRoses i={7} {...{ expanded, HandleActiveArtist }} />
-
-        {/* 
-          TODO: literally the rest. go through projects PDF.
-          - AubitSound
-            - apply links to rest of projects
-          - PlatinumRoses
-            - LINKS!!!
-          - ADIDAS!!! * ask jake about stuff here.
-        */}
+        {React.Children.map(children, (child, i) => {
+          return React.cloneElement(child, {
+            expanded,
+            setExpanded,
+            HandleActiveArtist,
+            i,
+          });
+        })}
       </div>
     </div>
   );
