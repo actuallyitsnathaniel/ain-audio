4eb7739b3a712a2268954ac3bbf98cc4683d4661
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-03-15 16:30:51 -0700

    hashmap out mar 18

--- src/routes/music/projects/riley.jsx
+++ src/routes/music/projects/riley.jsx
@@ -62,6 +62,12 @@ export const Riley = ({ i, expanded, HandleActiveArtist }) => {
               <h1 className="p-5">sound.xyz</h1>
               <div className="flex w-fit mx-auto p-5 bg-gray-500 rounded-lg bg-opacity-25 content-between gap-10">
                 <iframe
+                  src="https://embed.sound.xyz/v1/release/921354c1-e2a0-4d2f-bba3-76915cc6c45f?referral=0x35493e493e0d2001eda31bd7fb8859f961a227ce&referral_source=embed-sound"
+                  className="rounded-lg w-auto h-48"
+                  allow="clipboard-write"
+                  sandbox="allow-scripts allow-popups allow-popups-to-escape-sandbox"
+                ></iframe>
+                <iframe
                   src="https://embed.sound.xyz/v1/release/324ade13-93db-4f39-8f38-bb99b6085e07?referral=0x35493e493e0d2001eda31bd7fb8859f961a227ce&referral_source=embed-sound"
                   className="rounded-lg w-auto h-48"
                   allow="clipboard-write"
